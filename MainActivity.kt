9d9ac85c621c816761d0e80e6398e7f83ca3ff80
ishika-789 <82879259+ishika-789@users.noreply.github.com>
2021-06-29 18:43:58 +0530

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.myhealthapp
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity(),SensorEventListener {
+    var running =false
+    var sensorManager: SensorManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        var stepsSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        if(stepsSensor == null){
+            Toast.makeText( this,  "No Step Counter Sensor!",Toast.LENGTH_SHORT).show()
+        }else{
+            sensorManager?.registerListener( this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        running = false
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+}
